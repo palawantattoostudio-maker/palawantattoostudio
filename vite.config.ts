88adb463a4020e6642c154eddc502b7600a3acb2
palawan_tattoo_studio <palawantattoostudio@gmail.com>
2026-07-25 01:32:02 +0800

    l

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/palawantattoostudio/',
   plugins: [react()],
   resolve: {
     alias: {
